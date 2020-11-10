8949e548c4f5311a70fe37400c85371e844689b8
dayoung <amour090@naver.com>
2020-11-10 17:56:59 +0900

    gittest

--- app/src/main/java/com/example/teamproject/WriteCommunity.java
+++ app/src/main/java/com/example/teamproject/WriteCommunity.java
@@ -24,7 +24,7 @@ public class WriteCommunity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back);
         tb.setTitleTextColor(Color.BLACK);
 
-
+/*
         final CheckBox cb1 = (CheckBox)findViewById(R.id.ckFemale);
         final CheckBox cb2 = (CheckBox)findViewById(R.id.chMale);
         final CheckBox cb3 = (CheckBox)findViewById(R.id.ch10);
@@ -32,7 +32,7 @@ public class WriteCommunity extends AppCompatActivity {
         final CheckBox cb5 = (CheckBox)findViewById(R.id.ch30);
         final CheckBox cb6 = (CheckBox)findViewById(R.id.ch40);
         final CheckBox cb7 = (CheckBox)findViewById(R.id.chAddress);
-
+*/
         /*Button reg = (Button) findViewById(R.id.register);
         reg.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {

fae2990b77d60a08392caa3b844c00edf4af0e7d
dayoung <amour090@naver.com>
2020-11-16 01:53:26 +0900

    회원가입 & 로그인 데이터베이스 추가 기능

--- app/src/main/java/com/example/teamproject/Join.java
+++ app/src/main/java/com/example/teamproject/Join.java
@@ -6,11 +6,30 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Join extends AppCompatActivity {
+    public EditText join_Name;
+    public EditText join_resnum;
+    public EditText join_Number;
+    public EditText join_ID;
+    public EditText join_PW;
+    public EditText join_PwCheck;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +41,24 @@ public class Join extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.left_arrow);
         tb.setTitleTextColor(Color.BLACK);
 
+        join_Name = (EditText) findViewById(R.id.joinName);
+        join_resnum = (EditText) findViewById(R.id.joinresnum);
+        join_Number = (EditText) findViewById(R.id.joinNumber);
+        join_ID = (EditText) findViewById(R.id.joinID);
+        join_PW = (EditText) findViewById(R.id.joinPW);
+        join_PwCheck = (EditText) findViewById(R.id.joinPwCheck);
         Button join = (Button) findViewById(R.id.joinFin);
+
         join.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference();
+                myRef.child("user").push().setValue(join_Name.getText().toString());
+                myRef.child("user").push().setValue(join_resnum.getText().toString());
+                myRef.child("user").push().setValue(join_Number.getText().toString());
+                myRef.child("user").push().setValue(join_ID.getText().toString());
+                myRef.child("user").push().setValue(join_PW.getText().toString());
+                myRef.child("user").push().setValue(join_PwCheck.getText().toString());
                 int id = v.getId();
                 Intent it = new Intent(getApplicationContext(), Address.class);
                 startActivity(it);
@@ -42,5 +76,3 @@ public class Join extends AppCompatActivity {
     }
 
 }
-
-
--- app/src/main/java/com/example/teamproject/Login.java
+++ app/src/main/java/com/example/teamproject/Login.java
@@ -4,17 +4,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class Login extends AppCompatActivity implements View.OnClickListener{
+
+    EditText loginID, loginPW;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        firebaseAuth =  FirebaseAuth.getInstance();
+        loginID = findViewById(R.id.username);
+        loginPW = findViewById(R.id.password);
 
         Button loginB = findViewById(R.id.loginB);
         TextView findIDB = findViewById(R.id.findmyinfo);
@@ -26,27 +41,41 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         joinFinB.setOnClickListener(this);
         testWriteB.setOnClickListener(this);
     }
-    @Override
-    public void onClick(View v){
-        switch(v.getId()){
-            case R.id.loginB://로그인버튼
-                Intent it = new Intent(getApplicationContext(), Address.class);
-                startActivity(it);
-                break;
-            case R.id.findmyinfo://아이디/비밀번호 찾기
-                Intent it2 = new Intent(getApplicationContext(), FindMyInfo.class);
-                startActivity(it2);
-                break;
-            case R.id.membership://회원가입
-                Intent it3 = new Intent(getApplicationContext(), Join.class);
-                startActivity(it3);
-                break;
-            case R.id.testButton://커뮤니티 글쓰기 -> 나중에 지우기(테스트용)
-                Intent it4 = new Intent(getApplicationContext(), Mypage.class);
-                startActivity(it4);
-                break;
-            default:
-                break;
+        @Override
+        public void onClick (View v) {
+            switch (v.getId()) {
+                case R.id.loginB://로그인버튼
+                    String id = loginID.getText().toString().trim();
+                    String pwd = loginPW.getText().toString().trim();
+                    firebaseAuth.signInWithEmailAndPassword(id,pwd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(Login.this, Mainpage.class);
+                                startActivity(intent);
+
+                            }else{
+                                Toast.makeText(Login.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                    Intent it = new Intent(getApplicationContext(), Address.class);
+                    startActivity(it);
+                    break;
+                case R.id.findmyinfo://아이디/비밀번호 찾기
+                    Intent it2 = new Intent(getApplicationContext(), FindMyInfo.class);
+                    startActivity(it2);
+                    break;
+                case R.id.membership://회원가입
+                    Intent it3 = new Intent(getApplicationContext(), Join.class);
+                    startActivity(it3);
+                    break;
+                case R.id.testButton://커뮤니티 글쓰기 -> 나중에 지우기(테스트용)
+                    Intent it4 = new Intent(getApplicationContext(), CommunityMain.class);
+                    startActivity(it4);
+                    break;
+                default:
+                    break;
+            }
         }
-    }
 }
